c17acec0208a6cec93e5473aba2e19ccd256b716
SurviShirodkar22 <survishirodkarss12@gmail.com>
2023-11-29 02:31:18 +0530

    cards carousel

--- _assets/_js/index.js
+++ _assets/_js/index.js
@@ -56,7 +56,6 @@ const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
           ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
       : top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
   };
-  
   // Function to handle the visibility of elements
   function handleVisibility() {
     const elements = document.querySelectorAll('.in-viewport');
@@ -67,9 +66,59 @@ const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
       } 
     });
   }
-  
   // Event listener for scroll or any relevant event triggering visibility changes
   window.addEventListener('scroll', handleVisibility);
-  
   // Initial check on page load
   handleVisibility();
+
+
+
+  let slideIndex = 0;
+  const slidesToShow = 3;
+  const slides = document.querySelectorAll('.slide');
+  const slidesWrapper = document.querySelector('.slides');
+  const slideWidth = slidesWrapper.offsetWidth;
+  const slidesContainer = document.querySelector('.slides');
+  const totalSlides = slides.length;
+  
+  function changeSlide(n) {
+    slideIndex += n;
+    if (slideIndex >= totalSlides) {
+        slideIndex = 0;
+    } 
+    else if (slideIndex < 0) {
+        slideIndex = totalSlides - 1;
+    }
+
+    if (slideIndex + slidesToShow >= totalSlides) {
+        slidesContainer.style.transform = `translateX(-${(slideIndex + slidesToShow - totalSlides) * slideWidth/3}px)`;
+    } 
+    else {
+        slidesContainer.style.transform = `translateX(-${slideIndex * slideWidth/3}px)`;
+    }
+    
+    // Remove 'active' class from all slides
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides.forEach(slide => slide.classList.remove('left-slide-rotate'));
+    slides.forEach(slide => slide.classList.remove('right-slide-rotate'));
+
+    // Calculate the index of the middle slide
+    const middleSlideIndex = slideIndex + Math.ceil(slidesToShow / 2);
+    
+    // Add 'active' class to the middle slide
+    slides[middleSlideIndex].classList.add('active');
+    slides[middleSlideIndex-1].classList.add('left-slide-rotate');
+    slides[middleSlideIndex+1].classList.add('right-slide-rotate');
+  }
+  
+  // Show the first set of slides initially
+slides[2].classList.add('active');
+slides[1].classList.add('left-slide-rotate');
+slides[3].classList.add('right-slide-rotate');
+changeSlide(0);
+// setInterval(() => {
+//   changeSlide(1);
+// }, 3000);
+
+  
+
