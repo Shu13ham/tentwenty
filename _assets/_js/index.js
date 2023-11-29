604d33feab6891cc786eb4b48eb65da8db9d6e61
SurviShirodkar22 <survishirodkarss12@gmail.com>
2023-11-29 21:37:01 +0530

    Slider animation

--- _assets/_js/index.js
+++ _assets/_js/index.js
@@ -1,53 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
+  hamburgerToggle();
+  navbarToggle();
+  preloaderSetting();
+  checkViewportVisibility();
+});
+
+function hamburgerToggle() {
   const toggleButton = document.querySelector(".toggle-button");
   const hamburgerIcon = toggleButton.querySelector(".hamburger-icon");
   const crossIcon = toggleButton.querySelector(".close-icon");
   const navbarMobile = document.querySelector(".navbar-mobile");
 
   toggleButton.addEventListener("click", function () {
-    if (hamburgerIcon.style.display !== "none") {
-        document.body.style.overflow = 'hidden';
-        hamburgerIcon.style.display = "none";
-        crossIcon.style.display = "block";
-        navbarMobile.style.display = "block";
-        navbarMobile.classList.add("fade-left-animation");
+    const isHamburgerHidden = hamburgerIcon.style.display === "none";
+
+    document.body.style.overflow = !isHamburgerHidden ? "hidden" : "";
+    hamburgerIcon.style.display = !isHamburgerHidden ? "none" : "block";
+    crossIcon.style.display = !isHamburgerHidden ? "block" : "none";
+
+    if (!isHamburgerHidden) {
+      navbarMobile.style.display = "block";
+      navbarMobile.classList.add("fade-left-animation");
     } else {
-        document.body.style.overflow = '';
-        crossIcon.style.display = "none";
-        hamburgerIcon.style.display = 'block';
-        navbarMobile.classList.remove("fade-left-animation");
-        navbarMobile.classList.add("fade-right-animation");
-        setTimeout(() => {
-            navbarMobile.style.display = "none";
-            navbarMobile.classList.remove("fade-right-animation");
-        }, 500);
+      navbarMobile.classList.remove("fade-left-animation");
+      navbarMobile.classList.add("fade-right-animation");
+      setTimeout(() => {
+        navbarMobile.style.display = "none";
+        navbarMobile.classList.remove("fade-right-animation");
+      }, 500);
     }
-  });
-});
-
-window.addEventListener('scroll', function() {
-    const navElement = document.querySelector('nav');
-    if (navElement) {
-      if (window.scrollY > 0  && screen.width >=768) {
-        navElement.style.margin = '0px';
-      } 
-      else if(window.scrollY < 10  && screen.width >=768){
-        navElement.style.margin = '20px 20px 0';
-      }
-    }
-  });
-
-const preloader = document.querySelector("#preloader");
-if (preloader) {
-  window.addEventListener('load', () => {
-    preloader.style.opacity = "0";
-    setTimeout(() => {
-        preloader.remove();
-      }, 1000);
   });
 }
 
-const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
+function navbarToggle() {
+  window.addEventListener("scroll", function () {
+    const navElement = document.querySelector("nav");
+    if (navElement) {
+      if (window.scrollY > 0 && screen.width >= 768) {
+        navElement.style.margin = "0px";
+      } else if (window.scrollY < 10 && screen.width >= 768) {
+        navElement.style.margin = "20px 20px 0";
+      }
+    }
+  });
+}
+
+function preloaderSetting() {
+  const preloader = document.querySelector("#preloader");
+  if (preloader) {
+    window.addEventListener("load", () => {
+      preloader.style.opacity = "0";
+      setTimeout(() => {
+        preloader.remove();
+      }, 1000);
+    });
+  }
+}
+
+function checkViewportVisibility() {
+  const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
     const { top, left, bottom, right } = el.getBoundingClientRect();
     const { innerHeight, innerWidth } = window;
     return partiallyVisible
@@ -58,67 +69,69 @@ const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
   };
   // Function to handle the visibility of elements
   function handleVisibility() {
-    const elements = document.querySelectorAll('.in-viewport');
-  
-    elements.forEach(element => {
+    const elements = document.querySelectorAll(".in-viewport");
+
+    elements.forEach((element) => {
       if (elementIsVisibleInViewport(element)) {
-        element.classList.add('active');
-      } 
+        element.classList.add("active");
+      }
     });
   }
   // Event listener for scroll or any relevant event triggering visibility changes
-  window.addEventListener('scroll', handleVisibility);
+  window.addEventListener("scroll", handleVisibility);
   // Initial check on page load
   handleVisibility();
+}
 
+let slideIndex = 0;
+const slidesToShow = 3;
+const slides = document.querySelectorAll(".slide");
+const slidesWrapper = document.querySelector(".slides");
+const slideWidth = slidesWrapper.offsetWidth;
+const slidesContainer = document.querySelector(".slides");
+const totalSlides = slides.length;
 
-
-  let slideIndex = 0;
-  const slidesToShow = 3;
-  const slides = document.querySelectorAll('.slide');
-  const slidesWrapper = document.querySelector('.slides');
-  const slideWidth = slidesWrapper.offsetWidth;
-  const slidesContainer = document.querySelector('.slides');
-  const totalSlides = slides.length;
-  
-  function changeSlide(n) {
-    slideIndex += n;
-    if (slideIndex >= totalSlides) {
-        slideIndex = 0;
-    } 
-    else if (slideIndex < 0) {
-        slideIndex = totalSlides - 1;
-    }
-
-    if (slideIndex + slidesToShow >= totalSlides) {
-        slidesContainer.style.transform = `translateX(-${(slideIndex + slidesToShow - totalSlides) * slideWidth/3}px)`;
-    } 
-    else {
-        slidesContainer.style.transform = `translateX(-${slideIndex * slideWidth/3}px)`;
-    }
-    
-    // Remove 'active' class from all slides
-    slides.forEach(slide => slide.classList.remove('active'));
-    slides.forEach(slide => slide.classList.remove('left-slide-rotate'));
-    slides.forEach(slide => slide.classList.remove('right-slide-rotate'));
-
-    // Calculate the index of the middle slide
-    const middleSlideIndex = slideIndex + Math.ceil(slidesToShow / 2);
-    
-    // Add 'active' class to the middle slide
-    slides[middleSlideIndex].classList.add('active');
-    slides[middleSlideIndex-1].classList.add('left-slide-rotate');
-    slides[middleSlideIndex+1].classList.add('right-slide-rotate');
+function changeSlide(n) {
+  slideIndex += n;
+  if (slideIndex >= totalSlides) {
+    slideIndex = 0;
+  } else if (slideIndex < 0) {
+    slideIndex = totalSlides - 1;
   }
-  
-  // Show the first set of slides initially
-slides[2].classList.add('active');
-slides[1].classList.add('left-slide-rotate');
-slides[3].classList.add('right-slide-rotate');
+
+  if (slideIndex + slidesToShow >= totalSlides) {
+    slidesContainer.style.transform = `translateX(-${
+      ((slideIndex + slidesToShow - totalSlides) * slideWidth) / 3
+    }px)`;
+  } else {
+    slidesContainer.style.transform = `translateX(-${
+      (slideIndex * slideWidth) / 3
+    }px)`;
+  }
+
+  // Remove 'active' class from all slides
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides.forEach((slide) => slide.classList.remove("left-slide-rotate"));
+  slides.forEach((slide) => slide.classList.remove("right-slide-rotate"));
+  slides.forEach((slide) => slide.classList.remove("left-most-slide-rotate"));
+  slides.forEach((slide) => slide.classList.remove("right-most-slide-rotate"));
+
+  // Calculate the index of the middle slide
+  const middleSlideIndex = slideIndex + Math.ceil(slidesToShow / 2);
+
+  // Add 'active' class to the middle slide
+  slides[middleSlideIndex].classList.add("active");
+  slides[middleSlideIndex - 1].classList.add("left-slide-rotate");
+  slides[middleSlideIndex + 1].classList.add("right-slide-rotate");
+  slides[middleSlideIndex - 2].classList.add("left-most-slide-rotate");
+  slides[middleSlideIndex + 2].classList.add("right-most-slide-rotate");
+}
+
+// Show the first set of slides initially
+slides[2].classList.add("active");
+slides[1].classList.add("left-slide-rotate");
+slides[3].classList.add("right-slide-rotate");
 changeSlide(0);
 // setInterval(() => {
 //   changeSlide(1);
 // }, 3000);
-
-  
-
